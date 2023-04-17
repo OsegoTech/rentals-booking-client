c21ec59746710ddd8a6554183ed4a65c6b04e1ad
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-04-17 16:45:32 +0300

    Header Design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,36 @@
 import "./header.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBed, faCar, faPlane, faTaxi} from "@fortawesome/free-solid-svg-icons";
+
 const Header = () => {
     return(
-        <div className="header">Header</div>
+        <div className="header">
+            <div className="headerContainer">
+                <div className="headerList">
+                    <div className="headerListItem active">
+                        <FontAwesomeIcon icon={faBed}/>
+                        <span>Stays</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faPlane}/>
+                        <span>Flights</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faBed}/>
+                        <span>Attractions</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faTaxi}/>
+                        <span>Airport taxis</span>
+                    </div>
+                </div>
+                <h1 className="headerTitle">A lifetime of discounts? It's Genius."</h1>
+                <p className="headerDesc">
+                    Get rewarded for your travels - Unlock instant savings of 10% or more with a free Osego Bookings account
+                </p>
+                <button className="headerBtn">Sign in / Register</button>
+            </div>
+        </div>
     )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import "./home.css"
 import Navbar from "../../components/navbar/Navbar.jsx";
+import Header from "../../components/header/Header.jsx";
 const Home = () => {
     return(
         <div>
             <Navbar/>
+            <Header/>
         </div>
     )
 }
