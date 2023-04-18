ebc7d256d62042152fc1514435f1c7f9dfae91ec
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-04-18 17:19:23 +0300

    Designed the hompage listings

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,25 @@
 import "./home.css"
 import Navbar from "../../components/navbar/Navbar.jsx";
 import Header from "../../components/header/Header.jsx";
+import Featured from "../../components/featured/Featured.jsx";
+import PropertyList from "../../components/propertyList/PropertyList.jsx";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties.jsx";
 const Home = () => {
     return(
         <div>
             <Navbar/>
             <Header/>
+            <div className="homeContainer">
+                <Featured/>
+                <h1 className="homeTitle">
+                    Browse by property type
+                </h1>
+                <PropertyList/>
+                <h1 className="homeTitle">
+                    Homes guests love
+                </h1>
+                <FeaturedProperties/>
+            </div>
         </div>
     )
 }
