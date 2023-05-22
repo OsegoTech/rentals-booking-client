3fb34ba5b9ed548c741598b07be4ac5c5a36df4b
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-05-22 22:06:49 +0300

    configured axios to fetch backend data

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,35 +1,80 @@
-import "./featured.css"
+import useFetch from "../../hooks/useFetch";
+// import { useEffect, useState } from "react";
+import "./featured.css";
 const Featured = () => {
-    return(
-        <div className="featured">
-            <div className="featuredItem">
-                <img src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=" alt="" className="featuredImg"/>
-                <div className="featuredTitles">
-                    <h1>Dublin</h1>
-                    <h2>123 Properties</h2>
-                </div>
-            </div>
-            <div className="featuredItem">
-                <img src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o=" alt="" className="featuredImg"/>
-                <div className="featuredTitles">
-                    <h1>Reno</h1>
-                    <h2>533 Properties</h2>
-                </div>
-            </div>
-            <div className="featuredItem">
-                <img
-                    src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
-                    alt=""
-                    className="featuredImg"
-                />
-                <div className="featuredTitles">
-                    <h1>Austin</h1>
-                    <h2>532 properties</h2>
-                </div>
-            </div>
-        </div>
+  // const [data , setData] = useState([]);
+  // const [loading, setLoading] = useState(false);
 
-    )
-}
+  // Take note
+  const { data, loading, error } = useFetch(
+    "http://localhost:8800/api/hotels/countByCity?cities=Berlin,London,Madrid"
+  );
+  console.log(data);
 
-export default Featured
+  // end of take note
+  // useEffect(() => {
+  //   setLoading(true);
+  //   fetch("http://localhost:8800/api/hotels/countByCity?cities=Berlin,London,Madrid",{
+  //   method:"GET"
+  // }).then(response => {
+  //   if (!response.ok) {
+  //     throw new Error('Request failed');
+  //   }
+  //   return response.json();
+  // })
+  // .then(data => {
+  //   setData(data);
+  //   console.log(data);
+  // })
+  // .catch(error => {
+  //   console.error(error);
+  // });
+  //   setLoading(false);
+  // }, []);
+  return (
+    <div className="featured">
+      {loading ? (
+        "Loading Please Wait"
+      ) : (
+        <>
+          <div className="featuredItem">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Dublin</h1>
+              <strong>{data[0]}</strong>
+              <h2> properties</h2>
+            </div>
+          </div>
+          <div className="featuredItem">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Madrid</h1>
+              <h2>Properties</h2>
+            </div>
+          </div>
+          <div className="featuredItem">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>London</h1>
+              <h2> properties</h2>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Featured;
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+// import { json } from "stream/consumers";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url, {
+          headers: {
+            Authorization: "Bearer your_token",
+            "Content-Type": "application/json",
+          },
+        });
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetch };
+};
+
+export default useFetch;
